0eed7f994cb6bfdc6f141e624ccc59046072ace0
Cristiano <cristiano.costa@bcrservicos.com.br>
2022-05-05 10:55:02 -0300

    instalando exrtensão nodemailer para envio de email

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { prisma } from './prisma'
 
 const app = express()
 
@@ -12,9 +13,17 @@ app.use(express.json())
 // PATCH = ATUALIZAR UMA INFORMAÇÃO UNICA DA ENTIDADE
 // DELETE = DELETAR UMA INFORMAÇÃO
 
-app.post('/feedbacks', (request, response) => {
-  console.log(request.body)
-  return response.status(201).json(request.body)
+app.post('/feedbacks', async (request, response) => {
+  
+  const { type , comment , screenshot} = request.body;
+
+    const feedeback =  await  prisma.feedback.create({
+    data: {
+      comment,
+      type,
+      screenshot
+    }})
+  return response.status(201).json(feedeback)
 })
 
 app.listen(3333, () => console.log('SERVER IS RUNNING IN PORT 3333 🚀'))
