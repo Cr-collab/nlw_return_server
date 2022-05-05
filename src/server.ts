0275ce291f1a12f747380707a977388a031e9530
Cristiano <cristiano.costa@bcrservicos.com.br>
2022-05-05 11:33:43 -0300

    fazendo nosso envio de email

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma'
 
 const app = express()
@@ -13,6 +14,16 @@ app.use(express.json())
 // PATCH = ATUALIZAR UMA INFORMAÇÃO UNICA DA ENTIDADE
 // DELETE = DELETAR UMA INFORMAÇÃO
 
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "c731211408f155",
+    pass: "32d69137c2d4ff"
+  }
+});
+
+
 app.post('/feedbacks', async (request, response) => {
   
   const { type , comment , screenshot} = request.body;
@@ -23,7 +34,21 @@ app.post('/feedbacks', async (request, response) => {
       type,
       screenshot
     }})
-  return response.status(201).json(feedeback)
+
+
+    await transport.sendMail({
+      from: "Equipe BCR <oi@feedget.com>",
+      to: "Cristiano Azevedo <cristiccorrea@gami.com>",
+      subject: "Novo  feedback",
+      html: [
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+        `<p> Tipo do feedback ${type}</p>`,
+        `<p> Comentário : ${comment}</p>`,
+        `</div>`
+      ].join('\n')
+    })
+    
+  return response.status(201).json(request.body)
 })
 
 app.listen(3333, () => console.log('SERVER IS RUNNING IN PORT 3333 🚀'))
